38f07e33fa3f5d907514f4b72b21d82e15ee4175
Artem Levchenko <evgn.main@gmail.com>
2024-02-05 09:23:19 +0200

    Product class

---
+++ Sana07/ClassLibrary1/Product.cs
@@ -0,0 +1,60 @@
+﻿namespace ClassLibrary1;
+
+public abstract class Product
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new Exception("Name can\'t be null");
+            }
+
+            _name = value;
+        }
+    }
+    private decimal _price;
+
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new Exception("Price can\'t be less than 0");
+            }
+
+            _price = value;
+        }
+
+    }
+    private int _count;
+
+    public int Count
+    {
+        get => _count;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new Exception("Don't have it");
+            }
+
+            _count = value;
+        }
+    }
+    
+    public Product(string name, decimal price, int count)
+    {
+        _name = name;
+        _price = price;
+        _count = count;
+    }
+    
+    public abstract void Display();
+}
